155c4ee4f32cdae1dd91d8f4a4b9b13e81fa4b6b
Guilherme Montenegro <guimontenegro23@yahoo.com.br>
2024-11-05 13:16:50 -0300

    Laço for

--- Program.cs
+++ Program.cs
@@ -2,17 +2,29 @@
 using ExemploFundamentos.Models;
 
 
-Calculadora calc = new Calculadora();
+
+for (int i = 0; i <= 10; i++)
+{
+    for (int j = 1; j <= 10; j++)    
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    
+}
 
 
-int subtracao = calc.Subtracao(120, 321);
-int multiplicacao = calc.Multiplicacao(120, 321);
-double divisao = calc.Divisao(230, 25);
 
-Console.WriteLine(calc.Soma(120, 321));
-Console.WriteLine(subtracao);
-Console.WriteLine(multiplicacao);
-Console.WriteLine(divisao.ToString("F4", CultureInfo.InvariantCulture));
+// Calculadora calc = new Calculadora();
+
+
+// int subtracao = calc.Subtracao(120, 321);
+// int multiplicacao = calc.Multiplicacao(120, 321);
+// double divisao = calc.Divisao(230, 25);
+
+// Console.WriteLine(calc.Soma(120, 321));
+// Console.WriteLine(subtracao);
+// Console.WriteLine(multiplicacao);
+// Console.WriteLine(divisao.ToString("F4", CultureInfo.InvariantCulture));
 
 
 
