13eb43ea34d41c047e2e580b8dfc9d6b40053394
Guilherme Montenegro <guimontenegro23@yahoo.com.br>
2024-11-05 13:07:18 -0300

    Classes e instâncias

---
+++ Models/Calculadora.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploFundamentos.Models
+{
+    public class Calculadora
+    {
+        public string Soma(int x, int y)
+        {
+            return $"A Soma de {x} + {y} é igual a: {x+y}";
+        }
+
+        public int Subtracao(int x, int y)
+        {
+            return x - y;
+        }
+
+        public int Multiplicacao(int x, int y)
+        {
+            return x * y;
+        }
+
+        public double Divisao(double x, double y)
+        {
+            return x / y;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+using ExemploFundamentos.Models;
+
+
+Calculadora calc = new Calculadora();
+
+
+int subtracao = calc.Subtracao(120, 321);
+int multiplicacao = calc.Multiplicacao(120, 321);
+double divisao = calc.Divisao(230, 25);
+
+Console.WriteLine(calc.Soma(120, 321));
+Console.WriteLine(subtracao);
+Console.WriteLine(multiplicacao);
+Console.WriteLine(divisao.ToString("F4", CultureInfo.InvariantCulture));
+
+
+
+
+
+
+
+
+
+
+
+
+// Pessoa p = new Pessoa();
+
+// p.Nome = "Guilherme";
+// p.Idade = 33;
+
+// p.Apresentar();
