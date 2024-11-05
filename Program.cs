e454afed2a57732fe9a17a571fe9ab2cf674ca1e
Guilherme Montenegro <guimontenegro23@yahoo.com.br>
2024-11-05 13:51:38 -0300

    Switch case com While

--- Program.cs
+++ Program.cs
@@ -1,20 +1,64 @@
 ﻿using System.Globalization;
 using ExemploFundamentos.Models;
 
+string opcao;
 
-
-int soma = 0, numero = 0;
-
-do
+while (true)
 {
-    System.Console.Write("Digite um número (0 para parar): ");
-    numero = Convert.ToInt32(Console.ReadLine());
+    System.Console.WriteLine("Digite uma opção:");  
+    System.Console.WriteLine("1 - Cadastrar Cliente");
+    System.Console.WriteLine("2 - Buscar Cliente");
+    System.Console.WriteLine("3 - Apagar Cliente");
+    System.Console.WriteLine("4 - Encerrar");
 
-    soma += numero;
+    opcao = Console.ReadLine();
 
-} while (numero != 0);
+    switch (opcao)
+    {
+        case "1":
+            System.Console.WriteLine("Cadastrar cliente");
+            break;
+        
+        case "2":
+            System.Console.WriteLine("Buscar cliente");
+            break;
 
-System.Console.WriteLine($"A soma dos números é: {soma}");
+        case "3":
+            System.Console.WriteLine("Apagar cliente");
+            break;
+
+        case "4":
+            System.Console.WriteLine("Programa encerrado");
+            Environment.Exit(0);
+            break;
+
+        default:
+            System.Console.WriteLine("Opção inválida");
+            break;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+// int soma = 0, numero = 0;
+
+// do
+// {
+//     System.Console.Write("Digite um número (0 para parar): ");
+//     numero = Convert.ToInt32(Console.ReadLine());
+
+//     soma += numero;
+
+// } while (numero != 0);
+
+// System.Console.WriteLine($"A soma dos números é: {soma}");
 
 
 
